1b7d6a9a439bcc2c2dd5bb48f9e434945be5cff3
Phil Sofia <phil.sofia@gmail.com>
2016-03-13 16:35:50 -0400

    Implement starting board with 2s and 4s

--- js/application.js
+++ js/application.js
@@ -1,19 +1,30 @@
+
+
+
 function game() {
   this.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
   this.generateStartBoard = function(){
-    var found = 0;
-    while (found < 2){
-      var found = 0;
-      var board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-      var rand1 = Math.floor(Math.random() * board.length);
-      var rand2 = Math.floor(Math.random() * board.length);
-      var rand3 = Math.floor(Math.random() * board.length);
-      var rand4 = Math.floor(Math.random() * board.length);
-      board[rand1][rand2] = 2;
-      board[rand3][rand4] = 2;
+    var found, board;
+    while (found !== 2){
+      found = 0;
+      board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+      var randIndex1 = Math.floor(Math.random() * board.length);
+      var randIndex2 = Math.floor(Math.random() * board.length);
+      var randIndex3 = Math.floor(Math.random() * board.length);
+      var randIndex4 = Math.floor(Math.random() * board.length);
+      var startingValues = [2,2,2,2,2,2,2,2,2,4];
+      var randStartingValue1 = startingValues[Math.floor(Math.random() * startingValues.length)];
+      var randStartingValue2 = startingValues[Math.floor(Math.random() * startingValues.length)];
+      board[randIndex1][randIndex2] = randStartingValue1;
+      board[randIndex3][randIndex4] = randStartingValue2;
       for(var i = 0; i < board.length; i++) {
-        if (($.inArray(2, board[i])) !== -1){
-          found++;
+        for(var j = 0; j < board[i].length; j++) {
+          if (board[i][j] === 2){
+            found++;
+          }
+          if (board[i][j] === 4){
+            found++;
+          }
         }
       }
     }
@@ -28,27 +39,24 @@ function view() {
   this.displayBoard = function(board){
     var boardHTML = "";
     for(var i = 0; i < board.length; i++) {
-      console.log("i loop");
       boardHTML += '<tr>';
         for(var j = 0; j < board[i].length; j++) {
-          console.log("j loop");
           boardHTML += '<td>';
           boardHTML += board[i][j].toString();
           boardHTML += '</td>';
         }
       boardHTML += '</tr>';
     }
-    return boardHTML;
+    document.getElementById("board").innerHTML = boardHTML;
   };
 
 }
 
-var currentGame = new game(); 
-var currentView = new view();
-var startBoard = currentGame.generateStartBoard();
-var boardGame = currentView.displayBoard(startBoard);
+var Game = new game(); 
+var View = new view();
+var startingBoard = Game.generateStartBoard();
+
 $(document).ready(function(){
-  $('#board').html(boardGame);
+  View.displayBoard(startingBoard);
 
-})
-
+});
